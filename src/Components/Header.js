28474f5c00a7e34f47676d6249679c06ae649212
Troy Clarke <38792525+troyclarke69@users.noreply.github.com>
2019-06-26 18:29:02 -0400

    Add files via upload

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
+	  
+	  
 class Header extends Component {
   render() {
 
     if(this.props.data){
-      var name = this.props.data.name;
+		 var name = this.props.data.name; 
       var occupation= this.props.data.occupation;
       var description= this.props.data.description;
-      var city= this.props.data.address.city;
+
       var networks= this.props.data.social.map(function(network){
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
